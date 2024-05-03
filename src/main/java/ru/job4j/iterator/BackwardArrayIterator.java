916b6360ea15293738fdb1206f61a3922ac2edd3
SvFoxy <sveta_4_you_@mail.ru>
2024-05-03 13:18:59 +0300

    1. Что такое итератор. [#4951 #493348]

--- src/main/java/ru/job4j/iterator/BackwardArrayIterator.java
+++ src/main/java/ru/job4j/iterator/BackwardArrayIterator.java
@@ -14,7 +14,7 @@ public class BackwardArrayIterator implements Iterator<Integer> {
 
     @Override
     public boolean hasNext() {
-        return point < data.length && point >= 0;
+        return point >= 0;
     }
 
     @Override
