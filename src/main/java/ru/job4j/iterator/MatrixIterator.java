56d0846228d2ae79cf1168ed1ee0e8e1a03cfaa5
SvFoxy <sveta_4_you_@mail.ru>
2024-05-13 11:46:19 +0300

    2. Итератор для двухмерного массива int[][] [#9539 #494221]

--- src/main/java/ru/job4j/iterator/MatrixIterator.java
+++ src/main/java/ru/job4j/iterator/MatrixIterator.java
@@ -18,7 +18,7 @@ public class MatrixIterator implements Iterator<Integer> {
             column = 0;
             row++;
         }
-        return row < data.length && column < data[row].length;
+        return row < data.length;
     }
 
     @Override
